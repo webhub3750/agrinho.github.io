5656f1b39caee3a19c4a2dca6b9a3fe32e19e2f6
Caio Studio <131014719+webhub3750@users.noreply.github.com>
2024-08-09 06:32:23 -0300

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+function mostrarAba(id) {
+  
+  var abas = document.getElementsByClassName("tab-content");
+  for (var i = 0; i < abas.length; i++) {
+    abas[i].style.display = "none";
+  }
+ 
+  var conteudo = document.getElementById(id);
+  conteudo.style.display = "block";
+}
